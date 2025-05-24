702d7ba4c58b8de4b970bdb0f4d923c7a0580dcc
Heet Jain <heetopt@gmail.com>
2025-05-24 11:09:07 +0530

    modified changes

--- 4. Multithreading/pattern.java
+++ 4. Multithreading/pattern.java
@@ -1,66 +1,115 @@
-// Print a pattern like */*/*/* using two threads 
+// // Print a pattern like */*/*/* using two threads 
+// public class pattern {
+//   public static void main(String[] args) {
+//     Object lock = new Object();
+
+//     StarThread star = new StarThread(lock);
+//     SlashThread slash = new SlashThread(lock);
+
+//     slash.start();
+//     star.start();
+
+//   }
+// }
+
+// class StarThread extends Thread{
+//   Object lock;
+//   StarThread(Object lock){
+//     this.lock = lock;
+//   }
+
+//   @Override
+//   public void run() {
+//     for (int i = 0; i < 4; i++) {
+//       System.out.print("*");
+//       try {
+//         Thread.sleep(10);
+//       } catch (InterruptedException e) {
+//         e.printStackTrace();
+//       }
+
+//       if(i<3){
+//         synchronized(lock){
+//           lock.notify();
+//           try {
+//             lock.wait();
+//           } catch (Exception e) {
+//             System.out.println(e);
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+
+// class SlashThread extends Thread{
+//   Object lock;
+//   SlashThread(Object lock){
+//     this.lock = lock;
+//   }
+
+//   @Override
+//   public void run() {
+//     for (int i = 0; i < 3; i++) {
+//       synchronized(lock){
+//         try {
+//           lock.wait();
+//         } catch (Exception e) {
+//           System.out.println(e);
+//         }
+//         System.out.print("/");
+//         lock.notify();
+//       }
+
+//     }
+//   }
+// }
+
+
+// */*/*/*
 public class pattern {
+
   public static void main(String[] args) {
-    Object lock = new Object();
+    Alternator alt = new Alternator();
+    Thread t1 = new Thread(() -> {
+      for (int i = 0; i < 4; i++) {
+        alt.printStar();
+      }
+    });
+    Thread t2 = new Thread(() -> {
+      for (int i = 0; i < 3; i++) {
+        alt.printSlash();
+      }
+    });
 
-    StarThread star = new StarThread(lock);
-    SlashThread slash = new SlashThread(lock);
-
-    slash.start();
-    star.start();
-
+    t1.start();
+    t2.start();
   }
 }
-
-class StarThread extends Thread{
-  Object lock;
-  StarThread(Object lock){
-    this.lock = lock;
-  }
-
-  @Override
-  public void run() {
-    for (int i = 0; i < 4; i++) {
-      System.out.print("*");
-      try {
-        Thread.sleep(10);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
+class Alternator {
+  private boolean star = true;
+  public synchronized void printStar(){
+    try {
+      while (!star) {
+        wait();
       }
-
-      if(i<3){
-        synchronized(lock){
-          lock.notify();
-          try {
-            lock.wait();
-          } catch (Exception e) {
-            System.out.println(e);
-          }
-        }
-      }
+      System.out.println("*");
+      star = false;
+      notify();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
   }
-}
-
-class SlashThread extends Thread{
-  Object lock;
-  SlashThread(Object lock){
-    this.lock = lock;
-  }
-
-  @Override
-  public void run() {
-    for (int i = 0; i < 3; i++) {
-      synchronized(lock){
-        try {
-          lock.wait();
-        } catch (Exception e) {
-          System.out.println(e);
-        }
-        System.out.print("/");
-        lock.notify();
+  public synchronized void printSlash(){
+    try {
+      while (star) {
+        wait();
       }
-
+      System.out.println("/");
+      star = true;
+      notify();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
   }
 }
